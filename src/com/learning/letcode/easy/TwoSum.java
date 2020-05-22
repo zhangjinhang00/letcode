0e8b6774bef07a56ceda32ebeb37d040ef967e5e
1747002392@qq.com <1747002392@qq.com>
2020-05-22 18:36:44 +0800

    first commit

---
+++ src/com/learning/letcode/easy/TwoSum.java
@@ -0,0 +1,35 @@
+package com.learning.letcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * 两数字之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] ints = twoSum(nums, target);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        int[] resultArr = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    resultArr[0] = i;
+                    resultArr[1] = j;
+                }
+            }
+        }
+        return resultArr;
+    }
+}
---
+++ src/com/learning/letcode/easy/addTwoNumbers.java
@@ -0,0 +1,57 @@
+package com.learning.letcode.easy;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class addTwoNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode headNode = new ListNode(0);
+        ListNode p = l1, q = l2, current = headNode;
+        // 进位
+        int cf = 0;
+        while (p != null || q != null) {
+            int x = p == null ? 0 : p.val;
+            int y = q == null ? 0 : q.val;
+            int sum = x + y + cf;
+            current.next = new ListNode(sum % 10);
+            cf = sum / 10;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+            current = current.next;
+        }
+        if (cf > 0) {
+            current.next = new ListNode(cf);
+        }
+        return headNode.next;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
