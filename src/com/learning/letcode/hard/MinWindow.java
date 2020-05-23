9ee1ba27ab6df51b13a7309e4c4c067eaf6b1dd2
1747002392@qq.com <1747002392@qq.com>
2020-05-23 19:09:38 +0800

    MinWindow

---
+++ src/com/learning/letcode/hard/MinWindow.java
@@ -0,0 +1,32 @@
+package com.learning.letcode.hard;
+
+/**
+ * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入: S = "ADOBECODEBANC", T = "ABC"
+ * 输出: "BANC"
+ * 说明：
+ * <p>
+ * 如果 S 中不存这样的子串，则返回空字符串 ""。
+ * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-window-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinWindow {
+    public static void main(String[] args) {
+
+    }
+
+    public static String minWindow(String s, String t) {
+        char[] sChars = s.toCharArray();
+        String result = "";
+        char[] tChars = t.toCharArray();
+        for (int i = 0; i < sChars.length; i++) {
+        }
+        return result;
+    }
+}
